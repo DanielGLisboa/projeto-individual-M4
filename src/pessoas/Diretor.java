1980d7bbfa848586cc8fddaaeec9f24b1e86f1f4
Daniel Lisboa <daniel.lisbooa@gmail.com>
2023-03-13 19:42:33 -0300

    Inserindo atributos, getters e setters da classe Diretor

--- src/pessoas/Diretor.java
+++ src/pessoas/Diretor.java
@@ -1,3 +1,21 @@
 package pessoas;
+import midias.Filme;
+import java.util.ArrayList;
+
 public class Diretor extends Pessoa {
+
+    private final ArrayList<Filme> filmesDirigidos = new ArrayList<>();
+
+    Diretor(String nome, String dataNascimento, String genero) {
+        super(nome, dataNascimento, genero);
+    }
+
+
+    public ArrayList<Filme> getFilmesDirigidos() {
+        return filmesDirigidos;
+    }
+
+    public void setFilmesDirigidos(Filme filme) {
+        this.filmesDirigidos.add(filme);
+    }
 }
