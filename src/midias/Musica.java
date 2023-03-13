33a5f0efd517dae42d71b1f873278f7d33fcc30a
Daniel Lisboa <daniel.lisbooa@gmail.com>
2023-03-13 20:13:19 -0300

    Inserindo atributos, getters e setters da classe Musica.

--- src/midias/Musica.java
+++ src/midias/Musica.java
@@ -1,3 +1,37 @@
 package midias;
 public class Musica extends Midia {
+
+    private final String artista;
+
+   private int nota;
+
+    Musica(double duracao, String artista, int nota,String titulo, String genero, int ano) {
+        super(titulo, genero, ano, duracao);
+        if (nota >= 1 && nota <= 5) {
+               this.nota = nota;
+        }
+        else {
+               throw new IllegalArgumentException("A nota precisa estar entre 1 e 5");
+        }
+        this.artista = artista;
+    }
+
+
+    public String getArtista() {
+        return artista;
+    }
+
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        if(nota >= 1 && nota <= 5) {
+            this.nota = nota;
+        }
+        else {
+            throw new IllegalArgumentException("A nota precisa estar entre 1 e 5");
+        }
+    }
 }
