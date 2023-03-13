48649807eaba1239eac49246f0aa211daaa83331
Daniel Lisboa <daniel.lisbooa@gmail.com>
2023-03-13 19:44:00 -0300

    Inserindo atributos, getters e setters da classe Usuario

--- src/pessoas/Usuario.java
+++ src/pessoas/Usuario.java
@@ -1,3 +1,18 @@
 package pessoas;
 public class Usuario extends Pessoa{
+    
+   private String email;
+
+   Usuario(String nome, String dataNascimento, String genero, String email){
+       super(nome, genero,dataNascimento);
+       this.email = email;
+   }
+
+   public String getEmail() {
+       return email;
+   }
+
+   public void setEmail(String email) {
+       this.email = email;
+   }
 }
