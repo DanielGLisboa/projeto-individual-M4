2d18a95ddfbc2e9a57e1ec11f200bfce555b077f
Daniel Lisboa <daniel.lisbooa@gmail.com>
2023-03-13 20:24:25 -0300

    Inserindo atributos, getters e setters da classe Playlist.

--- src/midias/Playlist.java
+++ src/midias/Playlist.java
@@ -1,2 +1,62 @@
+package midias;
+import java.util.ArrayList;
+
 public class Playlist {
+    
+    private final String tituloPlaylist;
+    private final ArrayList<Midia> midias = new ArrayList<>();
+    private String ordemExecucao;
+    private Midia midiaAtual, proximaMidia, midiaAnterior;
+
+    Playlist(String tituloPlaylist, String ordemExecucao){
+        this.ordemExecucao = ordemExecucao;
+        this.tituloPlaylist = tituloPlaylist;
+
+    }
+
+    public ArrayList<Midia> getMidias() {
+        return midias;
+    }
+
+    public void setMidias(Midia midias) {
+        this.midias.add(midias);
+    }
+
+    public Midia getMidiaAtual() {
+        return midiaAtual;
+    }
+
+    public void setMidiaAtual(Midia midiaAtual) {
+        this.midiaAtual = midiaAtual;
+    }
+
+    public Midia getProximaMidia() {
+        return proximaMidia;
+    }
+
+    public void setProximaMidia(Midia proximaMidia) {
+        this.proximaMidia = proximaMidia;
+    }
+
+    public Midia getMidiaAnterior() {
+        return midiaAnterior;
+    }
+
+    public void setMidiaAnterior(Midia midiaAnterior) {
+        this.midiaAnterior = midiaAnterior;
+    }
+
+    public String getTituloPlaylist() {
+        return tituloPlaylist;
+    }
+
+
+    public String getOrdemExecucao() {
+        return ordemExecucao;
+    }
+
+    public void setOrdemExecucao(String ordemExecucao) {
+        this.ordemExecucao = ordemExecucao;
+    }
+
 }
