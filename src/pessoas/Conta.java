5899b0bbac26d45b7bfdc1312100af0dd681f5f7
Daniel Lisboa <daniel.lisbooa@gmail.com>
2023-03-13 20:22:56 -0300

    Inserindo atributos, getters e setters da classe Conta.

--- src/pessoas/Conta.java
+++ src/pessoas/Conta.java
@@ -1,2 +1,22 @@
+package pessoas;
 public class Conta {
+    
+    private String  senha;
+
+    private final Usuario usuario;
+
+    Conta(Usuario usuario, String senha){
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
 }
