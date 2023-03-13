1f0ea3acfd7310221aeccd4e3425fc9b62bbc545
Daniel Lisboa <daniel.lisbooa@gmail.com>
2023-03-13 19:27:59 -0300

    Inserindo atributos, getters e setters da classe Pessoa

--- src/pessoas/Pessoa.java
+++ src/pessoas/Pessoa.java
@@ -1,3 +1,31 @@
 package pessoas;
 public class Pessoa {
+    private final String nome;
+    private String genero;
+    private final String dataNascimento;
+
+
+    Pessoa(String nome, String dataNascimento, String genero){
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.genero = genero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 }
