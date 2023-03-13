0e603ced860158572e0092ae1e47e7893510deb3
Daniel Lisboa <daniel.lisbooa@gmail.com>
2023-03-13 20:11:52 -0300

    Inserindo atributos, getters e setters da classe Premiacao.

--- src/pessoas/artistas/Premiacao.java
+++ src/pessoas/artistas/Premiacao.java
@@ -1,3 +1,32 @@
 package pessoas.artistas;
 public class Premiacao {
+    
+    private String titulo;
+    private final String categoria;
+
+    private final int ano;
+
+    Premiacao (String titulo, int ano, String categoria){
+        this.titulo = titulo;
+        this.ano = ano;
+        this.categoria = categoria;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+
+
+    public int getAno() {
+        return ano;
+    }
 }
