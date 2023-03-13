df7ac1517ae4304b9db424eec27f6ddecf70a5c6
Daniel Lisboa <daniel.lisbooa@gmail.com>
2023-03-13 20:09:48 -0300

    Inserindo atributos, getters e setters da classe Ator; Inserindo atributos, getters e setters da classe Musico.

--- src/pessoas/artistas/Ator.java
+++ src/pessoas/artistas/Ator.java
@@ -1,3 +1,19 @@
 package pessoas.artistas;
 public class Ator extends Artista {
+    
+    private String formacao;
+
+    Ator(String nome, String dataNascimento, String genero, String formacao) {
+        super(nome, dataNascimento, genero);
+        this.formacao = formacao;
+
+    }
+
+    public String getFormacao() {
+        return formacao;
+    }
+
+    public void setFormacao(String formacao) {
+        this.formacao = formacao;
+    }
 }
--- src/pessoas/artistas/Musico.java
+++ src/pessoas/artistas/Musico.java
@@ -1,3 +1,17 @@
 package pessoas.artistas;
 public class Musico extends Artista {
+    
+    private String  atuacao;
+
+    Musico(String nome, String dataNascimento, String genero, String atuacao){
+        super(nome, dataNascimento, genero);
+    }
+
+    public String getAtuacao() {
+        return atuacao;
+    }
+
+    public void setAtuacao(String atuacao) {
+    this.atuacao = atuacao;
+    }
 }
