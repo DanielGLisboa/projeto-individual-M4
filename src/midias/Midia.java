6e3cdecb1a373292ef30095307efc57274d42ea0
Daniel Lisboa <daniel.lisbooa@gmail.com>
2023-03-13 19:26:15 -0300

    Inserindo atributos, getters e setters da classe Midia

--- src/midias/Midia.java
+++ src/midias/Midia.java
@@ -1,4 +1,33 @@
 package midias;
 public class Midia {
+    private final String titulo, genero;
+    private final int ano;
+
+    private final double duracao;
+
+    Midia(String titulo, String genero, int ano, double duracao) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.ano = ano;
+        this.duracao = duracao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+
+    public String getGenero() {
+        return genero;
+    }
+
+
+    public int getAno() {
+        return ano;
+    }
+
+    public double getDuracao() {
+        return duracao;
+    }
 }
 
