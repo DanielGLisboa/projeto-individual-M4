ed9719837e96373bebab7b2065f134e7d6245ce9
Daniel Lisboa <daniel.lisbooa@gmail.com>
2023-03-13 20:32:31 -0300

    Inserção do método imprime_filme na classe Filme.

--- src/midias/Filme.java
+++ src/midias/Filme.java
@@ -1,7 +1,9 @@
 package midias;
+
 import pessoas.artistas.Ator;
 import pessoas.Produtor;
 import pessoas.Diretor;
+import pessoas.artistas.Artista;
 import java.util.ArrayList;
 
 public class Filme extends Midia {
@@ -16,8 +18,6 @@ public class Filme extends Midia {
 
     private final Diretor diretor;
 
-
-
     Filme(double duracao, String tipo, String titulo, String genero, int ano, Produtor produtor, Diretor diretor) {
         super(titulo, genero, ano, duracao);
         this.duracao = duracao;
@@ -25,7 +25,6 @@ public class Filme extends Midia {
         this.produtor = produtor;
         this.diretor = diretor;
     }
-
 
     public void setDuracao(double duracao) {
         this.duracao = duracao;
@@ -35,7 +34,6 @@ public class Filme extends Midia {
         return tipo;
     }
 
-
     public ArrayList<Ator> getElenco() {
         return elenco;
     }
@@ -44,13 +42,30 @@ public class Filme extends Midia {
         this.elenco.add(elenco);
     }
 
-
     public Produtor getProdutor() {
         return produtor;
     }
 
-
     public Diretor getDiretor() {
         return diretor;
+    }
+
+    public void imprime_filme() {
+
+        System.out.println("======================= Filme1 ========================");
+        System.out.println("Filme: " + getTitulo());
+        System.out.println("Estrelando : " + getElenco().get(0).getNome());
+        System.out.println("Gênero: " + getGenero());
+        System.out.println("Ano de lançamento: " + getAno());
+        System.out.println("Tipo: " + getTipo());
+        System.out.println("Duração em horas: " + getDuracao());
+        System.out.println("Dirigido por: " + getDiretor().getNome());
+        System.out.println("Produzido por: " + getProdutor().getNome());
+        System.out.println(" |||   Elenco ||| ");
+        for (Artista ator : getElenco()) {
+            System.out.println(ator.getNome());
+        }
+        System.out.println("======================== FIM =========================");
+
     }
 }
