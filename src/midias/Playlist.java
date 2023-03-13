3a83c4ad3b1648eaa4e0b62814790bd74e88e756
Daniel Lisboa <daniel.lisbooa@gmail.com>
2023-03-13 20:51:35 -0300

    Inserção dos metodos reproduzir_midia_atual, reproduzir_próxima_midia, reproduzir_midia_anterior e misturar_midias na classe Playlist.

--- src/midias/Playlist.java
+++ src/midias/Playlist.java
@@ -1,14 +1,17 @@
 package midias;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class Playlist {
-    
+
     private final String tituloPlaylist;
     private final ArrayList<Midia> midias = new ArrayList<>();
     private String ordemExecucao;
     private Midia midiaAtual, proximaMidia, midiaAnterior;
 
-    Playlist(String tituloPlaylist, String ordemExecucao){
+    Playlist(String tituloPlaylist, String ordemExecucao) {
         this.ordemExecucao = ordemExecucao;
         this.tituloPlaylist = tituloPlaylist;
 
@@ -50,13 +53,98 @@ public class Playlist {
         return tituloPlaylist;
     }
 
-
     public String getOrdemExecucao() {
         return ordemExecucao;
     }
 
     public void setOrdemExecucao(String ordemExecucao) {
         this.ordemExecucao = ordemExecucao;
+    }
+
+    public void reproduzir_midia_atual() {
+        if (getOrdemExecucao().equalsIgnoreCase("Normal")) {
+            setMidiaAtual(getMidias().get(0));
+            System.out.println("Reproduzindo: " + getMidiaAtual().getTitulo());
+
+            setProximaMidia(getMidias().get(getMidias().indexOf(getMidiaAtual()) + 1));
+            setMidiaAnterior(null);
+        } else if (getOrdemExecucao().equalsIgnoreCase("Inverso") || getOrdemExecucao().equalsIgnoreCase("Inversa")) {
+            setMidiaAtual(getMidias().get(getMidias().size() - 1));
+            System.out.println("Reproduzindo: " + getMidiaAtual().getTitulo());
+
+            setProximaMidia(getMidias().get(getMidias().indexOf(getMidiaAtual())));
+            setMidiaAnterior(getMidias().get(getMidias().indexOf(getMidiaAtual()) - 1));
+        } else if (getOrdemExecucao().equalsIgnoreCase("Aleatorio")
+                || getOrdemExecucao().equalsIgnoreCase("Aleatoria")) {
+            int aleatorio = new Random().nextInt(getMidias().size() - 1);
+
+            setMidiaAtual(getMidias().get(aleatorio));
+            System.out.println("Reproduzindo: " + getMidiaAtual().getTitulo());
+
+            if (getMidias().indexOf(getMidiaAtual()) < getMidias().size() - 1
+                    && getMidias().indexOf(getMidiaAtual()) > 0) {
+                setProximaMidia(getMidias().get(getMidias().indexOf(getMidiaAtual()) + 1));
+                setMidiaAnterior(getMidias().get(getMidias().indexOf(getMidiaAtual()) - 1));
+            } else if (getMidias().indexOf(getMidiaAtual()) == getMidias().size() - 1) {
+                setProximaMidia(getMidias().get(getMidias().indexOf(getMidiaAtual())));
+                setMidiaAnterior(getMidias().get(getMidias().indexOf(getMidiaAtual()) - 1));
+            } else if (getMidias().indexOf(getMidiaAtual()) == 0) {
+                setProximaMidia(getMidias().get(getMidias().indexOf(getMidiaAtual()) + 1));
+                setMidiaAnterior(getMidias().get(getMidias().indexOf(getMidiaAtual())));
+            }
+        }
+    }
+
+    public void reproduzir_proxima_midia() {
+        boolean finalizar = false;
+
+        while (finalizar == false) {
+            if (getMidias().indexOf(getProximaMidia()) >= getMidias().indexOf(getMidiaAtual())) {
+                setMidiaAtual(getProximaMidia());
+                System.out.println("Reproduzindo : " + getMidiaAtual().getTitulo());
+
+                if ((getMidias().indexOf(getMidiaAtual()) + 1 <= getMidias().size() - 1)) {
+                    setProximaMidia(getMidias().get(getMidias().indexOf(getMidiaAtual()) + 1));
+                } else {
+                    System.out.println("Ultima mídia");
+                    System.out.println("Não há proxima mídia");
+                }
+                setMidiaAnterior(getMidias().get(getMidias().indexOf(getMidiaAtual()) - 1));
+                finalizar = true;
+            }
+        }
+    }
+
+    public void reproduzir_midia_anterior() {
+        boolean finalizar = false;
+
+        while (finalizar == false) {
+            if (getMidiaAnterior() != null) {
+                setMidiaAtual(getMidiaAnterior());
+                System.out.println("Reproduzindo : " + getMidiaAtual().getTitulo());
+
+                if ((getMidias().indexOf(getMidiaAtual()) > 0)) {
+                    setMidiaAnterior(getMidias().get(getMidias().indexOf(getMidiaAtual()) - 1));
+                } else {
+                    System.out.println("Primeira mídia");
+                    System.out.println("Não há mídia anterior");
+                }
+
+                setProximaMidia(getMidias().get(getMidias().indexOf(getMidiaAtual()) + 1));
+                finalizar = true;
+            } else {
+                setProximaMidia(getMidias().get(getMidias().indexOf(getMidiaAtual()) + 1));
+                System.out.println("Primeira midia");
+                System.out.println("Não há mídia anterior");
+                finalizar = true;
+            }
+        }
+
+    }
+
+    public void misturar_midias() {
+        Collections.shuffle(midias);
+
     }
 
 }
