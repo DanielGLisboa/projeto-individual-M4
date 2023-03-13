d1cad4f27e804b2af42d90b1947270a3b4543149
Daniel Lisboa <daniel.lisbooa@gmail.com>
2023-03-13 19:39:55 -0300

    Inserindo atributos, getters e setters da classe Produtor

--- src/pessoas/Produtor.java
+++ src/pessoas/Produtor.java
@@ -1,3 +1,22 @@
 package pessoas;
+import midias.Filme;
+import java.util.ArrayList;
+
 public class Produtor extends Pessoa {
+    
+    private final ArrayList<Filme> filmesProduzidos = new ArrayList<>();
+
+    Produtor(String nome, String dataNascimento, String genero) {
+        super(nome, dataNascimento, genero);
+    }
+
+
+    public ArrayList<Filme> getFilmesProduzidos() {
+        return filmesProduzidos;
+    }
+
+    public void setFilmesProduzidos(Filme filme) {
+        this.filmesProduzidos.add(filme);
+    }
+
 }
