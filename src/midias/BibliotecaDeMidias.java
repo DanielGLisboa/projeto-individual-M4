ebba39bc22bad6dae4d2efc03392005f9a168761
Daniel Lisboa <daniel.lisbooa@gmail.com>
2023-03-13 20:26:33 -0300

    Inserindo atributos, getters e setters da classe BibliotecaDeMidias.

--- src/midias/BibliotecaDeMidias.java
+++ src/midias/BibliotecaDeMidias.java
@@ -1,2 +1,47 @@
+package midias;
+import pessoas.Usuario;
+import java.util.ArrayList;
+
 public class BibliotecaDeMidias {
+    
+    private final ArrayList<String> tiposMidia = new ArrayList<>();
+
+    private final ArrayList<Midia> midias = new ArrayList<>();
+
+    private final  Usuario  usuario;
+
+    private final ArrayList<Playlist> playlists = new ArrayList<>();
+
+    BibliotecaDeMidias(Usuario usuario){
+        this.usuario = usuario;
+    }
+    public ArrayList<String>getTipoMidia() {
+        return this.tiposMidia;
+    }
+
+    public void setTipoMidia(String tipoMidia) {
+            this.tiposMidia.add(tipoMidia);
+    }
+
+    public ArrayList<Midia> getMidias( ) {
+        return this.midias;
+    }
+
+    public void setMidias(Midia midia) {
+        this.midias.add(midia);
+    }
+
+
+    public Usuario getUsuario() {
+        return this.usuario;
+    }
+
+
+    public ArrayList<Playlist> getPlaylists() {
+        return playlists;
+    }
+
+    public void setPlaylists(Playlist playlists) {
+        this.playlists.add(playlists);
+    }
 }
